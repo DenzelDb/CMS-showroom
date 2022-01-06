db4f9376f612b43c827fca2e041c2c35f1c7308e
De bruyn Denzel (s120583) <denzel.debruyn@student.ap.be>
2022-01-06 23:13:07 +0100

    vast --graphql vindt data niet

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,49 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const AboutPage = () => {
+export const query = graphql`
+query {
+  wpPage(slug: {eq: "about"}) {
+    aboutPage {
+      headerAboutUs {
+        description
+        title
+        picture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(quality: 100)
+            }
+          }
+        }
+      }
+      mission {
+        description
+        title
+        bannerPicture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(quality: 100)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const AboutPage = ({data: { wpPage: { aboutPage: {headerAboutUs, mission}}}}) => {
+  const imageHeader = getImage(headerAboutUs.image.localFile)
+  const imageMission = getImage(mission.bannerPicture.localFile)
+
   return (
     <Layout pageTitle="About Us">
-      <p></p>
+      <div>
+        <div>
+          <h2>{headerAboutUs.title}</h2>
+        </div>  
+      </div> 
     </Layout>
   )
 }
--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -1,31 +1,64 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import { Link, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import Game from '../../components/game'
 
 export const query = graphql`
-    query {
-        allWpGame {
-            edges {
-                node {
-                    gameMeta {
-                        name
-                    }
-                id
-                slug
+query {
+    wpPage(slug: {eq: "games"}) {
+        gamesPage {
+          headerGames {
+            description
+            title
+            image {
+              localFile {
+                childImageSharp {
+                   gatsbyImageData(placeholder: BLURRED, quality: 100, layout: FULL_WIDTH)
+                  }
                 }
-            }   
+              }
+            }
+          }
         }
-    }
   
+    allWpGame {
+      edges {
+        node {
+          gameMeta {
+            name
+            releaseYear
+            cover {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(placeholder: BLURRED, transformOptions: {fit: CONTAIN})
+                }
+              }
+            }
+          }
+          slug
+          id
+        }
+      }
+    }
+  }
 `
 
-const GamesPage = ({data: {allWpGame: {edges}}}) => {
+const GamesPage = ({data: {allWpGame: {edges: gamesInfo}, wpPage: {gamesPage},},}) => {
+    const image = getImage(gamesPage.headerGames.image.localFile)
   return (
     <Layout pageTitle="Games of MGS showroom">
-      {edges.map((item) => {
-          const game = item.node.gameMeta;
-          return <Link to={`/games/${item.node.slug}/`}><p key={item.node.id}>{game.name}</p></Link>
-      })}
+        <GatsbyImage
+            image={image}
+            alt='no image found.'
+        />
+        <div>
+            <h2>{gamesPage.headerGames.title}</h2>
+            <div dangerouslySetInnerHTML={{__html: gamesPage.headerGames.description,}}/>
+        </div>
+            {gamesInfo.map(({node: game}) => {
+                <Game slug={`games/${game.slug}`} key={game.id} game={game}/>
+            })}
     </Layout>
   )
 }
